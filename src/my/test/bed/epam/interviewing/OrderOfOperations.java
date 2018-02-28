42a90912567b6bf3891d20f15e723e032b4c71b2
cosmo.kramer101@gmail.com <P@ssw0rd>
2018-02-28 15:49:36 -0800

    misc stuff

--- src/my/test/bed/epam/interviewing/OrderOfOperations.java
+++ src/my/test/bed/epam/interviewing/OrderOfOperations.java
@@ -11,5 +11,35 @@ public class OrderOfOperations {
 
         System.out.println(--c);
 
+        d(c(b(a())));
+
     }
+
+
+    private static A a() {
+        System.out.println("a");
+        return new A();
+    }
+
+    private static B b(A a) {
+        System.out.println("b");
+        return new B();
+    }
+
+
+    private static C c(B b) {
+        System.out.println("c");
+        return new C();
+    }
+
+    private static void d(C c) {
+        System.out.println("d");
+    }
+
+
+    private static class A {}
+    private static class B {}
+    private static class C {}
+
+
 }
